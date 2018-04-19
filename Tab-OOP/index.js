53746c9c7614e74aec01af7b9b836ca9644b197e
MLuminary <418037025@qq.com>
2018-04-19 22:44:05 +0800

    jquery版

--- Tab-OOP/index.js
+++ Tab-OOP/index.js
@@ -12,7 +12,7 @@
       triggerType: 'mouseover',//用来定义鼠标的触发类型
       effect: 'default',//用来定义内容的切换效果
       invoke: 1,//默认展示第几个tab
-      auto: 5000 //用来定义tab是否自动切换，指定的时间为切换时间
+      auto: false //用来定义tab是否自动切换，指定的时间为切换时间
     };
 
     //最终配置
@@ -20,9 +20,31 @@
       $.extend(this.config,config);
     }
 
+    //点击或触摸
     this.tabItems.on(this.config.triggerType === 'click'?'click':'mouseover',function(){
       __this.invoke($(this));
     })
+
+    //自动播放
+    if(this.config.auto){
+      //定义全局定时器
+      this.timer = null;
+
+      //计数器
+      this.loop = 0;
+      this.autopaly();
+
+      this.tab.hover(()=>{
+        window.clearInterval(this.timer)
+      },()=>{
+        this.autopaly();
+      })
+    }
+
+    //设置默认显示第几个
+    if(this.config.invoke > 1){
+      this.invoke(this.tabItems.eq(this.config.invoke - 1));
+    }
 
 
   };
@@ -31,6 +53,8 @@
 
 
   Tab.prototype = {
+
+    //事件驱动
     invoke(currentTab){
       let __this = this;
       //索引
@@ -40,13 +64,34 @@
       let effect = this.config.effect;
       let conItems = this.contentItems;
 
+      //切换效果
       if(effect === "fade"){
         conItems.eq(index).stop(true).fadeIn().addClass("current").siblings().fadeOut().removeClass("current");//取消列队动画，当前动画立即完成
       }else{
         conItems.eq(index).addClass("current").siblings().removeClass("current");
       }
+
+      //如果加入自动播放
+      if(this.config.auto){
+         this.loop = index;
+      }
       
+    },
+    autopaly(){
+
+      let __this = this;
+      let tabLength = this.tabItems.length;
+      //全局定时器
+      this.timer = window.setInterval(function(){
+
+        __this.loop >= tabLength - 1 ? __this.loop = 0 : __this.loop++;
+        
+        __this.invoke(__this.tabItems.eq(__this.loop));
+        
+      },this.config.auto)
+
     }
+
   };
 
   //注册 jQuery
