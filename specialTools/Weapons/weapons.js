10fe1d9cf4bb1366418b1b6597c263b60dca7d30
MLuminary <418037025@qq.com>
2018-05-12 11:05:47 +0800

    增加武器

--- specialTools/Weapons/weapons.js
+++ specialTools/Weapons/weapons.js
@@ -8,3 +8,16 @@ function codePointLength(text) {
 function length(str) {
   return [...str].length;
 }
+
+//返回指定字符串在字符串中的所有位置
+function findStr(strs, str) {
+  var arr = [];//存储字符出现的位置
+  var startIndex = 0, i = 0, flag = 0;
+  while (flag >= 0) {
+    arr[i++] = strs.indexOf(str, startIndex)
+    flag = arr[i - 1];
+    startIndex = arr[i - 1] + 1;
+  }
+  arr.pop();
+  return arr;
+}
