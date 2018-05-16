8f277d57d2fa2e90cfa991f2afc8a5530a328839
MLuminary <418037025@qq.com>
2018-05-16 23:48:09 +0800

    爬取8页慕课网前端课程信息

--- easySpider/imoocSpider/app.js
+++ easySpider/imoocSpider/app.js
@@ -11,47 +11,59 @@ http.createServer(app).listen(8080);
 
 app.get('/', function(req, res) {
   //抓取五页
-  var pages = [1,2,3,4,5,6,7,8]
-  var baseUrl = 'https://www.imooc.com/course/list';
-  var homeUrl = 'https://www.imooc.com/'
-  var topicUrls = [];
+  var pages = [1, 2, 3, 4, 5, 6, 7, 8];
+  var baseUrl = 'https://www.imooc.com/course/list/';
+  var homeUrl = 'https://www.imooc.com/';
   var count = 0;
 
   var fetchUrl = function(page, callback) {
-    count++
-    console.log("当前并发数", count);
+    count++;
+    console.log('当前并发数', count);
 
     var params = {
       c: 'fe',
       page: page
     };
 
-    superagent.get(baseUrl).query({ params: JSON.stringify(params) }).end(function(err, content){
+    superagent
+      .get(baseUrl)
+      .query(params)
+      .end(function(err, content) {
+        var topicUrls = [];
+        var $ = cheerio.load(content.text);
+        var courseCard = $('.course-card-container');
+        courseCard.each(function(index, element) {
+          var $element = $(element);
+          var title = $element.find('.course-card-name').text();
+          var href = url.resolve(
+            homeUrl,
+            $element.find('.course-card').attr('href')
+          );
 
-      var $ = cheerio.load(content.text);
-      var courseCard = $('.course-card-container');
-      courseCard.each(function(index, element){
-        var $element = $(element);
-        var title = $element.find('.course-card-name').text();
-        var href = url.resolve(homeUrl, $element.find('.course-card').attr('href')) 
-
-        topicUrls.push({
-          title: title,
-          href: href
-        })
-      })
-      callback(err, topicUrls)
-      // callback(err,topicUrls);
-    })
+          topicUrls.push({
+            title: title,
+            href: href
+          });
+        });
+        callback(err, topicUrls);
+        
+        count--;
+        console.log('释放并发数后当前并发数', count);
+        // callback(err,topicUrls);
+      });
   };
 
-  async.mapLimit(pages, 5, function(page, callback){
-    fetchUrl(page, callback);
-  },function(err, result){ 
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf8' });
-    res.end(JSON.stringify(result))
+  async.mapLimit(
+    pages,
+    5,
+    function(page, callback) {
+      fetchUrl(page, callback);
+    },
+    function(err, result) {
+      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf8' });
+      res.end(JSON.stringify(result));
 
-    // console.log(result);
-  })
-
+      // console.log(result);
+    }
+  );
 });
