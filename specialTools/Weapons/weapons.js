cd41aaee8c87b8935501fffd474e0f9c8ca1bf55
MLuminary <418037025@qq.com>
2018-06-03 13:52:42 +0800

    html 转义

--- specialTools/Weapons/weapons.js
+++ specialTools/Weapons/weapons.js
@@ -1,55 +1,57 @@
 //返回正确字符串的长度
 function codePointLength(text) {
-  var result = text.match(/[\s\S]/gu);
-  return result ? result.length : 0;
+  var result = text.match(/[\s\S]/gu)
+  return result ? result.length : 0
 }
 
 //返回正确字符串的长度
 function length(str) {
-  return [...str].length;
+  return [...str].length
 }
 
 //返回指定字符串在字符串中的所有位置
 function findStr(strs, str) {
-  var arr = [];//存储字符出现的位置
-  var startIndex = 0, i = 0, flag = 0;
+  var arr = [] //存储字符出现的位置
+  var startIndex = 0,
+    i = 0,
+    flag = 0
   while (flag >= 0) {
     arr[i++] = strs.indexOf(str, startIndex)
-    flag = arr[i - 1];
-    startIndex = arr[i - 1] + 1;
+    // 保存位置，如果是 -1 就不不会进入下一个循环
+    flag = arr[i - 1]
+    startIndex = arr[i - 1] + 1
   }
-  arr.pop();
-  return arr;
+  // 移除 -1
+  arr.pop()
+  return arr
 }
 
-//下载base64图片
+//base64转Blob
 function base64Img2Blob(code) {
-  console.log(1);
-  var parts = code.split(';base64,');
-  var contentType = parts[0].split(':')[1];
-  var raw = window.atob(parts[1]);
-  var rawLength = raw.length;
+  var parts = code.split(';base64,')
+  var contentType = parts[0].split(':')[1]
+  var raw = window.atob(parts[1])
+  var rawLength = raw.length
 
-  var uInt8Array = new Uint8Array(rawLength);
+  var uInt8Array = new Uint8Array(rawLength)
 
   for (var i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i);
+    uInt8Array[i] = raw.charCodeAt(i)
   }
 
-  return new Blob([uInt8Array], { type: contentType });
+  return new Blob([uInt8Array], { type: contentType })
 }
 //下载base64图片
 function downloadFile(fileName, content) {
+  var aLink = document.createElement('a')
+  var blob = base64Img2Blob(content) //new Blob([content]);
 
-  var aLink = document.createElement('a');
-  var blob = base64Img2Blob(content); //new Blob([content]);
+  var evt = document.createEvent('MouseEvents')
+  evt.initEvent('click', false, false) //initEvent 不加后两个参数在FF下会报错
+  aLink.download = fileName
+  aLink.href = URL.createObjectURL(blob)
 
-  var evt = document.createEvent("MouseEvents");
-  evt.initEvent("click", false, false);//initEvent 不加后两个参数在FF下会报错
-  aLink.download = fileName;
-  aLink.href = URL.createObjectURL(blob);
-
-  aLink.dispatchEvent(evt);
+  aLink.dispatchEvent(evt)
 }
 
 // 将对象数据转为 location.search 的数据格式
@@ -60,4 +62,31 @@ function param(data) {
     url += `&${k}=${encodeURIComponent(value)}`
   }
   return url ? url.substring(1) : ''
+}
+
+// html转义
+function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  }
+  return text.replace(/[&<>"']/g, function(m) {
+    return map[m]
+  })
+}
+// 将HTML转义为实体
+function escape(html){
+  var elem = document.createElement('div')
+  var txt = document.createTextNode(html)
+  elem.appendChild(txt)
+  return elem.innerHTML;
+}
+// 将实体转回为HTML
+function unescape(str) {
+  var elem = document.createElement('div')
+  elem.innerHTML = str
+  return elem.innerText || elem.textContent
 }
