a7b088fb1ca382fc0f37e957a7b1da2564ce22f2
MLuminary <418037025@qq.com>
2018-06-11 12:07:47 +0800

    二叉树排序

--- Algorithm/experiment/二叉树/算法原理/index.js
+++ Algorithm/experiment/二叉树/算法原理/index.js
@@ -28,7 +28,7 @@ function BinaryTree() {
       }
     }
   }
-  
+
   // 插入
   this.insert = function(key) {
     // 新建一个二叉树节点
@@ -42,16 +42,63 @@ function BinaryTree() {
     }
   }
 
-  this.inOrderTraverse = function(callback) {
-    
+  // 中序遍历子节点
+  var midOrderTraverseNode = function(node, callback) {
+    if (node !== null) {
+      // 遍历左子树
+      midOrderTraverseNode(node.left, callback)
+      // 打印值
+      callback(node.key)
+      // 遍历右子树
+      midOrderTraverseNode(node.right, callback)
+    }
+  }
+
+  this.midOrderTraverse = function(callback) {
+    if (root !== null) {
+      midOrderTraverseNode(root, callback)
+    }
+  }
+
+  // 前序遍历子节点
+  var preOrderTraverseNode = function(node, callback) {
+    if (node !== null) {
+      // 打印值
+      callback(node.key)
+      // 遍历左子树
+      preOrderTraverseNode(node.left, callback)
+      // 遍历右子树
+      preOrderTraverseNode(node.right, callback)
+    }
+  }
+
+  this.preOrderTraverse = function(callback) {
+    if (root !== null) {
+      preOrderTraverseNode(root, callback)
+    }
+  }
+
+  var backOrderTraverseNode = function(node, callback) {
+    if (node !== null) {
+      // 遍历左子树
+      backOrderTraverseNode(node.left, callback)
+      // 遍历右子树
+      backOrderTraverseNode(node.right, callback)
+      // 打印值
+      callback(node.key)
+    }
+  }
+
+  this.backOrderTraverse = function(callback) {
+    if (root !== null) {
+      backOrderTraverseNode(root, callback)
+    }
   }
 
   // 展现二叉树
   this.show = function() {
     console.log(root)
   }
-
-
 }
 
 var nodes = [8, 3, 10, 1, 6, 14, 4, 13]
@@ -62,4 +109,13 @@ nodes.forEach(item => {
   binaryTree.insert(item)
 })
 
-binaryTree.show()
+var callback = function(key) {
+  console.log(key)
+}
+// 中序遍历
+console.log('中序遍历')
+binaryTree.midOrderTraverse(callback)
+console.log('前序遍历')
+binaryTree.preOrderTraverse(callback)
+console.log('后序遍历')
+binaryTree.backOrderTraverse(callback)
