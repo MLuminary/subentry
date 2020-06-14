d66aa827693da70f138b1b959d5c6aefa1f3f816
MLuminary <418037025@qq.com>
2020-06-14 23:30:14 +0800

    chore: format

--- specialTools/checkType/checkType.js
+++
@@ -1,4 +0,0 @@
-function checkType(value){
-  var s = Object.prototype.toString.call(value);
-  return s.match(/\[object (.*?)\]/)[1].toLowerCase();
-}
--- vue 双向绑定/vue.js
+++ vue 双向绑定/vue.js
@@ -3,14 +3,14 @@
  * @param {正常dom节点} node
  */
 function nodeToFragment(node, vm) {
-  var flag = document.createDocumentFragment();
-  var child;
+  var flag = document.createDocumentFragment()
+  var child
   //child先赋值为node的第一个子节点，然后判断其是否存在
   while ((child = node.firstChild)) {
-    compile(child, vm); //先进行模板编译
-    flag.appendChild(child);
+    compile(child, vm) //先进行模板编译
+    flag.appendChild(child)
   }
-  return flag;
+  return flag
 }
 
 /**
@@ -21,34 +21,33 @@ function nodeToFragment(node, vm) {
 function compile(node, vm) {
   //如果节点类型为元素
   if (node.nodeType === 1) {
-    var attr = node.attributes;
+    var attr = node.attributes
     //解析属性
     for (var i = 0, l = attr.length; i < l; i++) {
       if (attr[i].nodeName == 'v-model') {
-        var name = attr[i].nodeValue; //获取到其绑定的属性名
+        var name = attr[i].nodeValue //获取到其绑定的属性名
         //监听其 input
-        node.addEventListener('input', function(e) {
+        node.addEventListener('input', function (e) {
           //动态改变实例中data属性的值
-          vm[name] = e.target.value; //通过访问器属性
-        });
-        node.value = vm[name]; //通过访问器属性
-        node.removeAttribute('v-model');
+          vm[name] = e.target.value //通过访问器属性
+        })
+        node.value = vm[name] //通过访问器属性
+        node.removeAttribute('v-model')
       }
     }
   }
   //如果节点为文本
-  var reg = /\{\{(.*)\}\}/;
+  var reg = /\{\{(.*)\}\}/
   if (node.nodeType === 3) {
     if (reg.test(node.nodeValue)) {
-      var name = RegExp.$1; //获取正则匹配的第一个字符串
-      name = name.trim(); //清除空格
-      node.nodeValue = vm[name];
+      var name = RegExp.$1 //获取正则匹配的第一个字符串
+      name = name.trim() //清除空格
+      node.nodeValue = vm[name]
     }
   }
   //为每一个与实例 data 相关联的属性添加 Watcher
-  new Watcher(vm, node, name);
+  new Watcher(vm, node, name)
 }
-
 
 /**
  *
@@ -58,26 +57,26 @@ function compile(node, vm) {
  */
 function Watcher(vm, node, name) {
   //将 Watcher 添加到了 Dep 中
-  Dep.target = this;
-  this.name = name;
-  this.node = node;
-  this.vm = vm;
-  this.update();
+  Dep.target = this
+  this.name = name
+  this.node = node
+  this.vm = vm
+  this.update()
   //保证 Dep 只有一个值
-  Dep.target = null;
+  Dep.target = null
 }
 
 Watcher.prototype = {
   //更新
-  update: function() {
-    this.get();
-    this.node.nodeValue = this.value; //将视图更新
+  update: function () {
+    this.get()
+    this.node.nodeValue = this.value //将视图更新
   },
   //获取data中的属性值
-  get: function() {
-    this.value = this.vm[this.name]; //触发访问器属性
-  }
-};
+  get: function () {
+    this.value = this.vm[this.name] //触发访问器属性
+  },
+}
 
 /**
  *
@@ -86,40 +85,39 @@ Watcher.prototype = {
  * @param {*属性值} val
  */
 function defineReactive(obj, key, val) {
-  var dep = new Dep();
+  var dep = new Dep()
   //将对象属性劫持，添加访问器属性
   Object.defineProperty(obj, key, {
-    get: function() {
+    get: function () {
       //添加订阅者
-      if (Dep.target) dep.addSub(Dep.target);
-      return val;
+      if (Dep.target) dep.addSub(Dep.target)
+      return val
     },
-    set: function(newVal) {
-      if (newVal === val) return;
-      val = newVal;
+    set: function (newVal) {
+      if (newVal === val) return
+      val = newVal
       //作为发布者发出通知
-      dep.notify();
-    }
-  });
+      dep.notify()
+    },
+  })
 }
-
 
 /**
  * 保存订阅者的全局对象
  */
 function Dep() {
-  this.subs = [];
+  this.subs = []
 }
 
 Dep.prototype = {
-  addSub: function(sub) {
-    this.subs.push(sub);
+  addSub: function (sub) {
+    this.subs.push(sub)
   },
-  notify: function(){
-    this.subs.forEach(function(sub){
-      sub.update();
+  notify: function () {
+    this.subs.forEach(function (sub) {
+      sub.update()
     })
-  }
+  },
 }
 
 /**
@@ -129,9 +127,9 @@ Dep.prototype = {
  */
 function observe(obj, vm) {
   //对obj的属性进行遍历
-  Object.keys(obj).forEach(function(key) {
-    defineReactive(vm, key, obj[key]);
-  });
+  Object.keys(obj).forEach(function (key) {
+    defineReactive(vm, key, obj[key])
+  })
 }
 
 /**
@@ -139,13 +137,13 @@ function observe(obj, vm) {
  * @param {*属性对象} options
  */
 function Vue(options) {
-  this.data = options.data;
-  var data = this.data;
+  this.data = options.data
+  var data = this.data
   //对Data中属性添加访问器属性
-  observe(data, this);
+  observe(data, this)
 
-  var id = options.el;
-  var dom = nodeToFragment(document.getElementById(id), this);
+  var id = options.el
+  var dom = nodeToFragment(document.getElementById(id), this)
   //编译完成后，将dom返回
-  document.getElementById(id).appendChild(dom);
+  document.getElementById(id).appendChild(dom)
 }
